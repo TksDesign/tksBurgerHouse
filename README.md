5d832002ab49e215cf375a0cc07b3b39097bd682
daphane <daphtiss@gmail.com>
2025-06-11 09:52:00 +0100

    premier version de mon site

---
+++ README.md
@@ -0,0 +1,1 @@
+# Application miroir de BurgerHouse by TksDesign
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div className="div  bg-amber-900">
+      <h1>Burger House</h1>  
+    </div>
+  )
+}
+
+export default App
